8ead563c27d70a3427108d8e02164fee69401bd4
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-05-20 17:02:38 +0900

    Feat: set starting and ending activity

--- TimeLiveActivity/TimeLiveActivity/ContentView.swift
+++ TimeLiveActivity/TimeLiveActivity/ContentView.swift
@@ -6,12 +6,24 @@
 //
 
 import SwiftUI
+import ActivityKit
 
 struct ContentView: View {
+    @State private var activity: Activity<TimeAttributes>? = nil
     var body: some View {
         VStack {
-            Button("start") {}
-            Button("end") {}
+            Button("start") {
+                let attributes = TimeAttributes()
+                let state = TimeAttributes.ContentState(restTime: Date.now)
+                
+                activity = try? Activity<TimeAttributes>.request(attributes: attributes, contentState: state, pushType: nil)
+            }
+            Button("end") {
+                let state = TimeAttributes.ContentState(restTime: Date.now)
+                Task {
+                    await activity?.end(using: state, dismissalPolicy:.immediate)
+                }
+            }
         }
         .font(.title)
         .buttonStyle(.bordered)
