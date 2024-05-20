4799e5dd4275a6c3348fb7ccafcfbaacac8ae2d4
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-05-20 16:26:14 +0900

    Feat: create project

---
+++ TimeLiveActivity/TimeLiveActivity/TimeLiveActivityApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeLiveActivityApp.swift
+//  TimeLiveActivity
+//
+//  Created by Groo on 5/20/24.
+//
+
+import SwiftUI
+
+@main
+struct TimeLiveActivityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
