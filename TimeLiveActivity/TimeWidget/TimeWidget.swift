49a703120b30ee90b08cc385c10fdec7b5c8f3e4
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-05-20 17:18:51 +0900

    Feat: set dynamic island view

--- TimeLiveActivity/TimeWidget/TimeWidget.swift
+++ TimeLiveActivity/TimeWidget/TimeWidget.swift
@@ -5,76 +5,36 @@
 //  Created by Groo on 5/20/24.
 //
 
+import ActivityKit
 import WidgetKit
 import SwiftUI
 
-struct Provider: TimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), emoji: "😀")
-    }
-
-    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), emoji: "😀")
-        completion(entry)
-    }
-
-    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
-
-        // Generate a timeline consisting of five entries an hour apart, starting from the current date.
-        let currentDate = Date()
-        for hourOffset in 0 ..< 5 {
-            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-            let entry = SimpleEntry(date: entryDate, emoji: "😀")
-            entries.append(entry)
-        }
-
-        let timeline = Timeline(entries: entries, policy: .atEnd)
-        completion(timeline)
-    }
-}
-
-struct SimpleEntry: TimelineEntry {
-    let date: Date
-    let emoji: String
-}
-
-struct TimeWidgetEntryView : View {
-    var entry: Provider.Entry
-
-    var body: some View {
-        VStack {
-            Text("Time:")
-            Text(entry.date, style: .time)
-
-            Text("Emoji:")
-            Text(entry.emoji)
-        }
-    }
-}
-
 struct TimeWidget: Widget {
-    let kind: String = "TimeWidget"
-
     var body: some WidgetConfiguration {
-        StaticConfiguration(kind: kind, provider: Provider()) { entry in
-            if #available(iOS 17.0, *) {
-                TimeWidgetEntryView(entry: entry)
-                    .containerBackground(.fill.tertiary, for: .widget)
-            } else {
-                TimeWidgetEntryView(entry: entry)
-                    .padding()
-                    .background()
+        ActivityConfiguration(for: TimeAttributes.self) { context in
+            
+        } dynamicIsland: { context in
+            DynamicIsland {
+                DynamicIslandExpandedRegion(.bottom) {
+                    Text("\(context.state.restTime)")
+                    Text("잔디 심을 시간")
+                    Button("click") {}
+                }
+            } compactLeading: {
+                Text("CL")
+            } compactTrailing: {
+                Text("CT")
+            } minimal: {
+                Text("Min")
             }
         }
-        .configurationDisplayName("My Widget")
-        .description("This is an example widget.")
     }
 }
 
-#Preview(as: .systemSmall) {
-    TimeWidget()
-} timeline: {
-    SimpleEntry(date: .now, emoji: "😀")
-    SimpleEntry(date: .now, emoji: "🤩")
+struct TimeWidgetView: View {
+    let context: ActivityViewContext<TimeAttributes>
+    
+    var body: some View {
+        Text(context.state.restTime, style: .relative)
+    }
 }
