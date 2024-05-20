9740674fbb513eded659aed3ca729a02cf790338
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-05-20 17:06:33 +0900

    Feat: create widget extension 'TimeWidget'

---
+++ TimeLiveActivity/TimeWidget/TimeWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  TimeWidgetBundle.swift
+//  TimeWidget
+//
+//  Created by Groo on 5/20/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct TimeWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        TimeWidget()
+    }
+}
